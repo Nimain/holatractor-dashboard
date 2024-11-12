e244021c8bcaebe23954953f8ef2ace257936ca8
Ankit-Mohanta <mohantaankit2002@gmail.com>
2024-11-12 15:36:01 +0530

    Add farm form updated

--- components/Dashboards/Farmer/FarmMapping/FarmMapping.tsx
+++ components/Dashboards/Farmer/FarmMapping/FarmMapping.tsx
@@ -5,27 +5,88 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 import { EditControl } from "react-leaflet-draw";
 import { useState, useEffect } from "react"
+import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { Textarea } from '@/components/ui/textarea';
+import { CircularProgress } from '@mui/material';
+import { errorMessage, successMessage } from '@/utils/Toastify/Messages';
+import { renderInstance } from '@/utils/Axios/RenderInstance';
+import { Button } from '@/components/ui/button';
+import { useCookie } from 'next-cookie';
 
 interface Location {
   latitude: number | null;
   longitude: number | null;
 }
 
-interface FarmMap {
-  layerType: string;
-  _latlngs: Location[]
+interface user {
+  userId: string;
+  image: string;
+  name: string;
+  email: string;
 }
 
 const FarmBooking = () => {
   const [error, setError] = useState<string | null>(null);
   const [location, setLocation] = useState<Location>({ latitude: null, longitude: null });
-  const [farmMap, setFarmMap] = useState<FarmMap | null>(null)
+  const [layerType, setLayerType] = useState("")
+  const [coordinates, setCoordinates] = useState<Location[]>([])
+  const [open, setOpen] = useState(false)
+
+  const [farmName, setFarmName] = useState("")
+  const [farmDescription, setFarmDescription] = useState("")
+
+  const [adding, setAdding] = useState(false)
+
+  const { cookie } = useCookie()
+  const user: user = cookie.get("user")
+  const access_token = cookie.get("access_token")
 
   const _created = (e: any) => {
-    setFarmMap(e)
+    setLayerType(e.layerType)
+    setCoordinates(e.layer._latlngs)
+    setOpen(true)
   }
 
-  console.log(farmMap)
+  function handleAddFarm() {
+    if (!layerType || coordinates.length === 0) {
+      errorMessage("Please check the area again")
+      return
+    }
+    if (!farmName) {
+      errorMessage("Please give the farm name")
+      return
+    }
+
+    setAdding(true)
+    renderInstance.post("/farm", {
+      owner_id: user.userId,
+      type: layerType,
+      name: farmName,
+      boundary: {
+        coordinates: [coordinates]
+      }
+    }, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      }
+    }).then(()=>{
+      successMessage("Farm added")
+    }).catch((err)=>{
+      if (err.response && err.response.status === 404 && err.response.data.message === "Booking is not valid") {
+        errorMessage("Booking is not valid")
+    } else if (err.response && err.response.status === 400 && err.response.data.message === "Booking already confirm") {
+        successMessage("Successfully booked")
+    } else if (err.response && err.response.status === 400 && err.response.data.message === "You are not allowed to perform this task") {
+        successMessage("You are not allowed to perform this task")
+    } else {
+        errorMessage("Some error occurred. Please try again...")
+    }
+    }).finally(()=>{setAdding(false)})
+  }
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -50,7 +111,11 @@ const FarmBooking = () => {
       {error ? (
         <p>Error: {error}</p>
       ) : (location.latitude && location.longitude) ? (
-        <MapContainer center={[location.latitude, location.longitude]} zoom={13} scrollWheelZoom={false} style={{ width: "100%", height: "100vh" }}>
+        <MapContainer 
+        center={[location.latitude, location.longitude]} 
+        zoom={13} 
+        scrollWheelZoom={false} 
+        style={{ width: "100%", height: "100vh", zIndex: 1 }}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -74,6 +139,41 @@ const FarmBooking = () => {
       ) : (
         <p>Latitude and longitude not available</p>
       )}
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="w-fit h-fit">
+
+          <Card className="w-full max-w-sm">
+            <CardHeader>
+              Give ffarm details
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <div className="space-y-4">
+                <Label>Farm name</Label>
+                <Input
+                  value={farmName}
+                  onChange={e => { setFarmName(e.target.value) }}
+                  required={true} />
+              </div>
+              <div className="space-y-4">
+                <Label>Farm descriiption</Label>
+                <Textarea
+                  value={farmDescription}
+                  onChange={e => { setFarmDescription(e.target.value) }}
+                  className="resize-none" />
+              </div>
+              <Separator />
+            </CardContent>
+            <CardFooter>
+              <Button onClick={() => { handleAddFarm() }}>
+                {adding && <CircularProgress />}
+                Save
+              </Button>
+            </CardFooter>
+          </Card>
+
+        </DialogContent>
+      </Dialog>
     </>
   )
 }
