7000ee158bd0d4dd3c5e9933ed701f6edb4197e9
unknown <nimain2412@gmail.com>
2025-03-06 00:36:11 -0800

    fixan error

--- utils/Axios/RenderInstance.ts
+++ utils/Axios/RenderInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const NestJsBaseURL = "https://farmmechanize.holatractor.com/api/"
 
 export const renderInstance = axios.create({
-    baseURL: "NestJsBaseURL",
+    baseURL: NestJsBaseURL,
     headers: {
         "Content-Type": "application/json",
     }
