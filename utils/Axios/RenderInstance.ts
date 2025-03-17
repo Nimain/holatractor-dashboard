27490e0277d27ad98fbf5a251ae694413f0bf0c1
Nimain <nimain@live.com>
2025-03-17 16:34:56 +0530

    Update RenderInstance.ts
    backend link render

--- utils/Axios/RenderInstance.ts
+++ utils/Axios/RenderInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const NestJsBaseURL = "https://farmmechanize.holatractor.com/api/"
+export const NestJsBaseURL = "https://holatractor-backend-render.onrender.com/api/"
 
 export const renderInstance = axios.create({
     baseURL: NestJsBaseURL,
