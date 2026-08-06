690a596a22a5fe7ab71002147b84373e9ed6315f
Nimain <nimain@live.com>
2026-08-07 03:23:26 +0530

    update server

--- utils/Axios/RenderInstance.ts
+++ utils/Axios/RenderInstance.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export const NestJsBaseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001/"
-export const DeviceBaseURL = "https://device.holatractor.com/"
-
-// export const NestJsBaseURL = "http://localhost:5000/"
+export const NestJsBaseURL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://holatractor-backend-render.onrender.com/";
+export const DeviceBaseURL = "https://device.holatractor.com/";
 
 export const renderInstance = axios.create({
   baseURL: NestJsBaseURL,
   headers: {
     "Content-Type": "application/json",
-  }
-}) 
+  },
+});
